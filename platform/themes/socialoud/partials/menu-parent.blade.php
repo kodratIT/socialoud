815c7dace5951ae6fb4777f7cd5cfa266d3476b0
kodratIT <kodratcoc@gmail.com>
2026-09-02 07:32:11 +0700

    fix socialoud category navigation refresh

--- platform/themes/socialoud/partials/menu-parent.blade.php
+++ platform/themes/socialoud/partials/menu-parent.blade.php
@@ -1,14 +1,30 @@
 @php
-    $parentCategories = collect($menu_nodes)->filter(fn ($row) => $row->reference_type === \Botble\Blog\Models\Category::class);
+    $categoryIds = collect($menu_nodes)
+        ->filter(fn ($row) => $row->reference_type === \Botble\Blog\Models\Category::class)
+        ->pluck('reference_id')
+        ->filter()
+        ->unique()
+        ->values();
+    $categories = \Botble\Blog\Models\Category::query()
+        ->whereIn('id', $categoryIds)
+        ->with('slugable')
+        ->get()
+        ->keyBy('id');
 @endphp
 
 <ul {!! $options !!}>
-    @foreach ($parentCategories as $row)
-        <li @class([$row->css_class, 'current' => $row->active])>
-            <a href="{{ url($row->url) }}" @if ($row->target !== '_self') target="{{ $row->target }}" @endif>
-                {!! $row->icon_html !!} <span>{{ $row->title }}</span>
-            </a>
-        </li>
+    @foreach ($menu_nodes as $row)
+        @php
+            $category = $categories->get($row->reference_id);
+            $isActive = $category && rtrim(url($category->url), '/') === rtrim(request()->url(), '/');
+        @endphp
+        @if ($row->reference_type === \Botble\Blog\Models\Category::class && $category)
+            <li @class([$row->css_class, 'current' => $isActive])>
+                <a href="{{ url($category->url) }}" @if ($row->target !== '_self') target="{{ $row->target }}" @endif>
+                    {!! $row->icon_html !!} <span>{{ $category->name }}</span>
+                </a>
+            </li>
+        @endif
     @endforeach
     @if (\Illuminate\Support\Facades\Route::has('public.galleries'))
         <li @class(['socialoud-gallery-menu-item', 'current' => request()->routeIs('public.galleries')])>
@@ -16,3 +32,4 @@
         </li>
     @endif
 </ul>
+
